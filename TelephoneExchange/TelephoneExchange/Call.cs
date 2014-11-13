0f81465aff39aac8e79215054abf0253eaa0aa00
VladCherniavsky <vlad.cherniavsky@gmail.com>
2014-11-13 03:27:53 +0300

    14
    Added "Call" class

---
+++ TelephoneExchange/TelephoneExchange/Call.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TelephoneExchange
+{
+    public class Call
+    {
+        private int _timeSpan;
+        public Client Caller { get; set; }
+        public Client Answerer { get; set; }
+        public DateTime TimeOfStartCall { get; set; }
+        public DateTime TimeOfFinishCall { get; set; }
+
+        public int DurationOfCall
+        {
+            get { return DurationOfCall; }
+            set { DurationOfCall = _timeSpan; }
+        }
+
+        public int CalculateDurationOfCall(DateTime startTime, DateTime finishTime)
+        {
+           int start =  startTime.Second;
+           int finish = finishTime.Second;
+           return _timeSpan = finish - start;
+        }
+
+        public void StartCall()
+        {
+            TimeOfStartCall = DateTime.Now;
+        }
+
+        public void EndCall(double value)
+        {
+            TimeOfFinishCall = DateTime.Now.AddMinutes(value);
+        }
+
+    }
+}
